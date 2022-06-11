04f967c157a8f05d12daa33f2093644d52794aff
Kun <kun.register@gmail.com>
2022-06-11 18:28:14 +0800

    remove unused code. apply secret to env file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
-from pprint import pprint
 from notion_client import Client
 from dotenv import load_dotenv
 import os
@@ -9,17 +8,12 @@ PROP_STARTED_AT = "Started At"
 PROP_ENDED_AT = "Ended At"
 
 load_dotenv()
-# print(os.environ["NOTION_TOKEN"])
 
 notion = Client(auth=os.environ["NOTION_TOKEN"])
-
-db_things = "8de9aae74c3d48f1b23e3f4c84f9637e"
-
-# db_records = "a8abde7d9df44f868ce3851a8fb66f78"
+db_things = os.environ["DB_THINGS"]
 
 now = datetime.now(timezone.utc)
-filter_time = now - timedelta(minutes=3)
-print(filter_time.isoformat())
+filter_time = now - timedelta(minutes=5)
 
 
 def query_last_edited(filter_time: datetime):
@@ -49,7 +43,6 @@ def query_last_edited(filter_time: datetime):
 
 def get_last_edited_taipei_time(page):
     time_string_len = len("2022-06-10T06:06:00")
-    # 2022-06-10T06:06:00.000Z
     last_edited_time = page['last_edited_time'][0:time_string_len]
 
     utc_time_with_tz = datetime.fromisoformat(
@@ -169,121 +162,3 @@ last_edit_results = query_last_edited(filter_time)
 
 for page in last_edit_results:
     parse_page(page)
-
-
-# def on_it_true_process(page):
-
-
-# last edit
-# [on it] is True -> (R) Record end time is empty -> do nothing -> otherwise, (C) create a new record
-# [on it] is False -> (R) if Record end time is empty -> (U) fill the end time -> otherwise, do nothing
-# response = query_last_edited(filter_time)
-# print(response)
-
-# response = notion.databases.query(
-#     database_id=db_things,
-#     filter={
-#         "property": "On It",
-#         "checkbox": {
-#             "equals": True
-#         }
-#     }
-# )
-
-# response = notion.databases.query(
-#     database_id=db_records,
-#     filter={
-#         "and":[
-#             {
-#                 "property": "Task",
-#                 "relation": {
-#                     "contains": "cd11a269daaa4ddd8bdac0b1c1341372"
-#                 }
-#             },
-#             {
-#                 "property": "End At",
-#                 "date": {
-#                     "is_empty": True # something is in progress
-#                 }
-#             },
-
-#         ]
-#     }
-# )
-
-# print(len(response["results"]))
-
-# pprint(response)
-
-# print(respfrom datetime import datetime
-# import json
-# response = json.loads(response)
-
-# print(response['object'])
-# results = response['results']
-
-# for m in results:
-    # notion.pages.create(
-    #     parent={
-    #         "type": "database_id",
-    #         "database_id": "a8abde7d9df44f868ce3851a8fb66f78"
-    #     },
-    #     title="hello",
-    #     properties={
-    #         "Name": {
-    #             "title": [
-    #                 {
-    #                     "text": {
-    #                         "content": "GOGOGO"
-    #                     }
-    #                 }
-    #             ]
-    #         },
-    #         "Time Frame": {
-    #             "date": {
-    #                 "start": datetime.now().isoformat(),
-    #                 "end": datetime.now().isoformat()
-    #             }
-    #         },
-    #         "Task": {
-    #             "relation": [
-    #                 {"id": m['id']}
-    #             ],
-    #         }
-    #     }
-    # )
-
-
-my_date = datetime.now()
-print(my_date.isoformat())
-
-
-# notion.pages.create(
-#     parent={
-#         "type": "database_id",
-#         "database_id": "a8abde7d9df44f868ce3851a8fb66f78"
-#     },
-#     title="hello",
-#     properties={
-#         "Name": {
-#             "title": [
-#                 {
-#                     "text": {
-#                         "content": "GOGOGO"
-#                     }
-#                 }
-#             ]
-#         },
-#         "Time Frame": {
-#             "date": {
-#                 "start": datetime.now().isoformat(),
-#                 "end": datetime.now().isoformat()
-#             }
-#         },
-#         "Task": {
-#             "relation": [
-#                 {"id": "9b1e95e7dc774d52acac7f7021468d2b"}
-#             ],
-#         }
-#     }
-# )
